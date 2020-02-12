3e02fafaf9ec9935a1a36cde1cd82d4c2d558aff
Andre R. Ferreira <deveras@users.noreply.github.com>
2020-02-12 11:53:10 +0000

    Add About page

--- source/js/pages/About/About.js
+++ source/js/pages/About/About.js
@@ -1,13 +1,128 @@
 import React from 'react';
 
-import './About.scss';
-
 function About () {
   return (
     <section>
-      <article>
-        <h1>Personal Biography</h1>
-      </article>
+      <section>
+        <article>
+          <h1>About me</h1>
+          <p>
+            I grew up during a time when doing silly things outside with your
+            mates was a thing. However, it didn't take long until the wonders of
+            technology seduced and drove me inside, thus transforming an
+            extrovert into a moderate introvert. This passion for technology is
+            a strong personal trait, one that led me to embarked in a European
+            professional life experience that shaped the person I am today.
+          </p>
+          <p>
+            Hi! I'm André, a trust wordy problem solver that adapts with ease
+            without topping a strong sense of responsibility. After dodging some
+            curve balls and climbing steep hills, I was lucky to find myself at
+            the top of Maslow's pyramid. Knowing where I was, I couldn't stop
+            myself. My phycological multipotentiality in tandem with my Einstein
+            like passionate curiosity, drove me from taking a Cyber Security
+            Masters from Northumbria University to attempting an MBA at{' '}
+            <span lang="pt-PT">Universidade de Coimbra</span>. During these
+            endeavours and despite a short-term memory of only a few gigabytes,
+            I managed to achieve things I never thought I would. Nonetheless,
+            this recent academic facet drove me away from the keyboard, the
+            place where hours fly without warning. This cursed blessing, only
+            really hurts when you lift your eyes and have a look around, but its
+            again time to forget time and have some fun. I hope you enjoy the
+            blog.
+          </p>
+          <p>
+            Professionally, I've performed in many roles with different levels
+            of responsibility, in many market verticals. I've worked in small
+            little shops of 2 programmers and in multi-cultural internationally
+            renowned web companies with hundreds, enjoying them all. I've played
+            with a lot of different technologies and shared professional
+            experiences with a lot of smart people from different parts of the
+            world while based in Portugal, Ireland, Spain and England. I’ve
+            travelled to closer and distant countries like Mexico and the US to
+            support customers and colleagues alike. I've experienced developing
+            applications for: Smart TVs, PlayStation, Nintendo, X-Box, Roku,
+            mobiles and desktops alike. I've done backend, I’ve done frontend,
+            I've used waterfall, Agile and Kanban, but time flies when I'm
+            programming.
+          </p>
+          <p>
+            Outside of the office, I enjoy wine tasting, repeating good meals,
+            watching games, driving around and cycling. Love playing Sid Meier's
+            Civilization VI at the moment. I speak a few languages and I'm
+            always eager to engage in conversation using any of them. I'm fluent
+            in English, Portuguese and French. I can also speak Spanish and
+            understand Italian, perhaps less common is the fact that I can
+            manage a tiny bit of Polish and a bit less Lithuanian, not
+            conversational but enough to get around (whenever I get to visit).
+            At a certain point in time I knew basic Irish sign language, but
+            I've forgot it now. I've also tried Chinese, but my ears couldn't
+            quite make out the difference between the sounds (I still find
+            'Portuguese people' hilarious <span lang="ce">(葡萄牙人)</span>).
+            Now a days renovating my grandparents' house in a frugal DIY fashion
+            to the extent that I can, but I can already share some stories and
+            show some remarkable progress for someone that didn't have a clue
+            what he was doing in the beginning (it's great to have friends and
+            the Internet).
+          </p>
+        </article>
+
+        <aside>
+          <h2>Latest positions</h2>
+          <ul>
+            <li>
+              <h3>ICT consultant</h3>
+              <p>Allied Irish Bank</p>
+              <p>Dublin, Ireland</p>
+            </li>
+            <li>
+              <h3>ICT consultant</h3>
+              <p>QlikTech</p>
+              <p>London, United Kingdom</p>
+            </li>
+            <li>
+              <h3>ICT consultant</h3>
+              <p>SKY</p>
+              <p>London, United Kingdom</p>
+            </li>
+            <li>
+              <h3>Web developer</h3>
+              <p>Amazon</p>
+              <p>London, United Kingdom</p>
+            </li>
+            <li>
+              <h3>ICT consultant </h3>
+              <p>O2 - Telefonica</p>
+              <p>Dublin, Ireland</p>
+            </li>
+          </ul>
+        </aside>
+      </section>
+
+      <section>
+        <article>
+          <h1>My Cyber Security disertation abstract</h1>
+          <h2>
+            An event-driven simulation on VANETs security using physical layer
+            security of wireless communications
+          </h2>
+          <p>
+            Vehicles exchanging information between themselves and road side
+            units supported by IEEE 802.11p will soon allow mankind to produce
+            new solutions to tackle traffic problems that have existed for
+            decades. To ensure and enable that Ad Hoc wireless communications
+            are secure, the dissertation presented a software artefact capable
+            of simulating their interactions when attempting to produce a
+            symmetric based private key with IEEE 802.11p physical layer
+            properties of wireless communication, additive white gaussian noise,
+            scatterers mobility and turbo codes for information reconciliation.
+            Supported on international standards best practices allied with an
+            Agile development methodology, the produced simulator allows for
+            further exploration of the most suitable settings to apply in
+            different traffic scenarios.
+          </p>
+        </article>
+      </section>
     </section>
   );
 }
--- source/js/pages/Contact/Contact.js
+++ source/js/pages/Contact/Contact.js
@@ -35,54 +35,56 @@ function Contact () {
   ];
 
   return (
-    <section id="contactPage">
-      <article>
-        <h1>Contact</h1>
+    <section>
+      <section id="contactPage">
+        <article>
+          <h1>Contact</h1>
 
-        <p>
-          I've created this space as to share some of my experience and thoughts
-          with you. I would love to know what you thought, however could you do
-          that using social media? You can find me at:
-        </p>
+          <p>
+            I've created this space as to share some of my experience and
+            thoughts with you. I would love to know what you thought, however
+            could you do that using social media? You can find me at:
+          </p>
 
-        <ul id="social">
-          {socialMediaItems.map((menuItem, iterator) => (
-            <li key={iterator}>
-              <ReactRouterDOM.Link
-                to={menuItem.link}
-                target="_blank"
-                title={menuItem.title}
-                hrefLang={menuItem.hreflang}
-              >
-                <img src={menuItem.imgSrc} />{' '}
-                <span>{menuItem.description}</span>
-              </ReactRouterDOM.Link>
-            </li>
-          ))}
-        </ul>
+          <ul id="social">
+            {socialMediaItems.map((menuItem, iterator) => (
+              <li key={iterator}>
+                <ReactRouterDOM.Link
+                  to={menuItem.link}
+                  target="_blank"
+                  title={menuItem.title}
+                  hrefLang={menuItem.hreflang}
+                >
+                  <img src={menuItem.imgSrc} />{' '}
+                  <span>{menuItem.description}</span>
+                </ReactRouterDOM.Link>
+              </li>
+            ))}
+          </ul>
 
-        <h1>Public speaking</h1>
-        <p>
-          You would like to level up your development team? I can to talk
-          extensively in an entertaining fashion supported by carefully craft
-          presentations.
-          <br />
-          If you are running an event and need someone to fillin the gaps, I
-          might be up for that too.
-        </p>
+          <h1>Public speaking</h1>
+          <p>
+            You would like to level up your development team? I can to talk
+            extensively in an entertaining fashion supported by carefully craft
+            presentations.
+            <br />
+            If you are running an event and need someone to fillin the gaps, I
+            might be up for that too.
+          </p>
 
-        <h1>Press</h1>
-        <p>
-          If you are interested, check out some of my recent media appearances
-          on print.
-          <br />
-          Waiting for them to be published... stay tuned!
-        </p>
-      </article>
+          <h1>Press</h1>
+          <p>
+            If you are interested, check out some of my recent media appearances
+            on print.
+            <br />
+            Waiting for them to be published... stay tuned!
+          </p>
+        </article>
 
-      <aside id="contactForm">
-        <ContactForm />
-      </aside>
+        <aside id="contactForm">
+          <ContactForm />
+        </aside>
+      </section>
     </section>
   );
 }
