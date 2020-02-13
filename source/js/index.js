f79f61a1980d7c935ff9d02fc802e27e073c5cdb
Andre R. Ferreira <deveras@users.noreply.github.com>
2020-02-13 13:46:06 +0000

    Add no match route

--- source/js/index.js
+++ source/js/index.js
@@ -14,6 +14,7 @@ import Resource from './pages/Resource/Resource';
 import Resources from './pages/Resources/Resources';
 import Tutorial from './pages/Tutorial/Tutorial';
 import Tutorials from './pages/Tutorials/Tutorials';
+import NoMatch from './pages/NoMatch/NoMatch';
 
 function App () {
   return (
@@ -39,6 +40,7 @@ function App () {
             path="/tutorials/:articleId"
             component={Tutorial}
           />
+          <ReactRouterDOM.Route component={NoMatch} />
         </ReactRouterDOM.Switch>
       </React.StrictMode>
     </ReactRouterDOM.BrowserRouter>
---
+++ source/js/pages/NoMatch/NoMatch.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './NoMatch.scss';
+
+function NoMatch () {
+  return (
+    <section>
+      <article>
+        <h1>Route not found....</h1>
+      </article>
+    </section>
+  );
+}
+
+export default NoMatch;
